78d04088cd67dd23f4054aeab87f4a24164b6574
DaviCarozo <marianecarozo@gmail.com>
2025-05-12 15:55:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('acceptButton').addEventListener('click', function() {
+    alert('Você aceitou os termos de serviço!');
+    // Aqui você pode redirecionar o usuário ou guardar a aceitação em algum local.
+    // Exemplo: redirecionar para outra página
+    // window.location.href = 'pagina_de_aceitacao.html';
+});
